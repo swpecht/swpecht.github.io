20cef1828ab77b88ef1ba599a14b3db5d639ff98
Steven Pecht <steven.pecht@gmail.com>
2021-11-10 11:50:28 -0800

    Change worldsize to be configurable

--- projects/running-emu/src/main.rs
+++ projects/running-emu/src/main.rs
@@ -1,8 +1,10 @@
 use std::{collections::{VecDeque}, hash::Hash};
 
+const WORLD_SIZE: usize = 15;
+
 #[derive(Clone, Copy)]
 struct World {
-    costs: [Option<i8>; 100],
+    costs: [Option<i8>; WORLD_SIZE * WORLD_SIZE],
     start: Point,
     goal: Point,
 }
@@ -14,7 +16,7 @@ struct Point {
 }
 
 fn new_world() -> World {
-    World { costs: [Some(0); 100], start: Point {x: 0, y: 0}, goal: Point{x: 9, y: 9} }
+    World { costs: [Some(0); WORLD_SIZE * WORLD_SIZE], start: Point {x: 0, y: 0}, goal: Point{x: 9, y: 9} }
 }
 
 fn main() {
@@ -26,8 +28,8 @@ fn main() {
 }
 
 fn print_world(world: World) {
-    for y in 0..10 {
-        for x in 0..10 {
+    for y in 0..WORLD_SIZE {
+        for x in 0..WORLD_SIZE {
             let p = Point{x: x, y: y};
 
             if p == world.start {
@@ -35,7 +37,7 @@ fn print_world(world: World) {
             } else if p == world.goal {
                 print!("G")
             } else {
-                print!("{}", world.costs[y * 10 + x].unwrap_or(-1))
+                print!("{}", world.costs[y * WORLD_SIZE + x].unwrap_or(-1))
             }
             print!("\t")            
         }
@@ -52,13 +54,14 @@ fn find_path_bfs(world: World) -> Vec<Point> {
     return vec![world.start]
 }
 
-fn get_distance_matrix(world: World) -> [Option<i8>; 100] {
-    let mut dmatrix: [Option<i8>; 100] = [None; 100];
+/// Return the distance to get to each point in the world from the starting point
+fn get_distance_matrix(world: World) -> [Option<i8>; WORLD_SIZE * WORLD_SIZE] {
+    let mut dmatrix: [Option<i8>; WORLD_SIZE * WORLD_SIZE] = [None; WORLD_SIZE * WORLD_SIZE];
     let mut queue: VecDeque<Point> = VecDeque::new();
 
     queue.push_back(world.start);
     let start = world.start;
-    dmatrix[start.y * 10 + start.x] = Some(0);
+    dmatrix[start.y * WORLD_SIZE + start.x] = Some(0);
 
     while !queue.is_empty() {
         let node = queue.pop_front().unwrap();
@@ -67,10 +70,10 @@ fn get_distance_matrix(world: World) -> [Option<i8>; 100] {
         }
 
         let neighors = get_neighbors(node);
-        let d = dmatrix[node.y * 10 + node.x].unwrap();
+        let d = dmatrix[node.y * WORLD_SIZE + node.x].unwrap();
         for n in neighors {
-            if dmatrix[n.y * 10 + n.x].is_none() {      
-                dmatrix[n.y * 10 + n.x] = Some(d+1);
+            if dmatrix[n.y * WORLD_SIZE + n.x].is_none() {      
+                dmatrix[n.y * WORLD_SIZE + n.x] = Some(d+1);
                 queue.push_back(n);
             }
         }
@@ -89,7 +92,7 @@ fn get_neighbors(point: Point) -> Vec<Point> {
 
     for d in directions {
         let candidate = Point {x: (point.x as i32 + d.0) as usize, y: (point.y as i32 + d.1) as usize};
-        if candidate.x < 10 && candidate.y < 10 {
+        if candidate.x < WORLD_SIZE && candidate.y < WORLD_SIZE {
             neighbors.push(candidate);
         }
     }
