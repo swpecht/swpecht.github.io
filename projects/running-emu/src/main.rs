847ea7890df97f505d06a9eb340335029fd17749
Steven Pecht <steven.pecht@gmail.com>
2021-11-10 11:46:02 -0800

    Add distance matrix calculation.

--- projects/running-emu/src/main.rs
+++ projects/running-emu/src/main.rs
@@ -1,23 +1,28 @@
+use std::{collections::{VecDeque}, hash::Hash};
+
+#[derive(Clone, Copy)]
 struct World {
-    costs: [i32; 100],
+    costs: [Option<i8>; 100],
     start: Point,
     goal: Point,
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Copy, Hash, Eq)]
 struct Point {
     x: usize,
     y: usize
 }
 
 fn new_world() -> World {
-    World { costs: [0; 100], start: Point {x: 0, y: 0}, goal: Point{x: 9, y: 9} }
+    World { costs: [Some(0); 100], start: Point {x: 0, y: 0}, goal: Point{x: 9, y: 9} }
 }
 
 fn main() {
     let world = new_world();
 
     print_world(world);
+    println!("");
+    find_path_bfs(world);
 }
 
 fn print_world(world: World) {
@@ -26,13 +31,68 @@ fn print_world(world: World) {
             let p = Point{x: x, y: y};
 
             if p == world.start {
-                print!("O")
+                print!("S")
             } else if p == world.goal {
-                print!("X")
+                print!("G")
             } else {
-                print!("{}", world.costs[y * 10 + x])
-            }            
+                print!("{}", world.costs[y * 10 + x].unwrap_or(-1))
+            }
+            print!("\t")            
         }
         println!("");
     }
+}
+
+/// Returns a vector of Points for the shortest path to the goal
+fn find_path_bfs(world: World) -> Vec<Point> {
+    let dmatrix = get_distance_matrix(world);
+    let dist_world = World{costs: dmatrix, ..world};
+    print_world(dist_world);
+
+    return vec![world.start]
+}
+
+fn get_distance_matrix(world: World) -> [Option<i8>; 100] {
+    let mut dmatrix: [Option<i8>; 100] = [None; 100];
+    let mut queue: VecDeque<Point> = VecDeque::new();
+
+    queue.push_back(world.start);
+    let start = world.start;
+    dmatrix[start.y * 10 + start.x] = Some(0);
+
+    while !queue.is_empty() {
+        let node = queue.pop_front().unwrap();
+        if node == world.goal {
+            break
+        }
+
+        let neighors = get_neighbors(node);
+        let d = dmatrix[node.y * 10 + node.x].unwrap();
+        for n in neighors {
+            if dmatrix[n.y * 10 + n.x].is_none() {      
+                dmatrix[n.y * 10 + n.x] = Some(d+1);
+                queue.push_back(n);
+            }
+        }
+        
+    }
+
+    return dmatrix
+    
+}
+
+fn get_neighbors(point: Point) -> Vec<Point> {
+    // Allow adjacent and diagonal movement
+    let directions = [(-1, 0), (1, 0), (0, -1), (0, 1), (1, 1), (-1, -1), (1, -1), (-1, 1)];
+    let mut neighbors: Vec<Point> = Vec::new();
+
+
+    for d in directions {
+        let candidate = Point {x: (point.x as i32 + d.0) as usize, y: (point.y as i32 + d.1) as usize};
+        if candidate.x < 10 && candidate.y < 10 {
+            neighbors.push(candidate);
+        }
+    }
+
+    return neighbors
 }
