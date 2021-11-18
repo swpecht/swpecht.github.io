367d55bdc34935e474c41f89e1d3d4446fadd209
Steven Pecht <steven.pecht@gmail.com>
2021-11-17 19:17:28 -0800

    Transition some calls to archetype ECS with filter

--- projects/running-emu/src/main.rs
+++ projects/running-emu/src/main.rs
@@ -1,11 +1,17 @@
 use crossterm::{
     event::{read, Event},
     execute,
-    style::{ResetColor, SetBackgroundColor},
+    style::{Color, ResetColor, SetBackgroundColor},
 };
 use running_emu::{
-    attacker_system_update, ecs::BackgroundHighlight, ecs::Point, ecs::Position, ecs::Sprite,
-    ecs::Visibility, ecs::World, print_cost_matrix, AttackerAgent,
+    attacker_system_update,
+    ecs::BackgroundHighlight,
+    ecs::Point,
+    ecs::Sprite,
+    ecs::Visibility,
+    ecs::World,
+    ecs::{ComponentType, Position},
+    print_cost_matrix, AttackerAgent,
 };
 use std::io::stdout;
 
@@ -31,7 +37,7 @@ fn main() {
 
     loop {
         num_steps += 1;
-        render_system_update(&world);
+        render_system_update(&mut world);
         if attacker_system_update(&mut world, &mut agent) {
             break;
         }
@@ -45,59 +51,82 @@ fn main() {
     // println!("Found in {} steps", steps);
 }
 
+#[derive(Clone)]
+struct RenderPoint {
+    c: char,
+    bg: Option<Color>,
+}
+
 /// Update the render of the player visible map
-fn render_system_update(world: &World) {
+fn render_system_update(world: &mut World) {
     // execute!(stdout(), Clear(ClearType::FromCursorDown)).unwrap();
 
     // Populate base layer
-    let mut output = vec![vec!['?'; world.width]; world.height];
+    let mut output = vec![vec![RenderPoint { c: '?', bg: None }; world.width]; world.height];
 
-    // Draw over top with entities
-    let positions = world.borrow_component_vec::<Position>().unwrap();
-    let sprites = world.borrow_component_vec::<Sprite>().unwrap();
-    let visibility = world.borrow_component_vec::<Visibility>().unwrap();
-    let zip = positions
-        .iter()
-        .zip(sprites.iter())
-        .zip(visibility.iter())
-        .map(|((p, s), v): ((&Option<Position>, &Option<Sprite>), &Option<Visibility>)| (p, s, v));
-    let drawable = zip.filter_map(
-        |(p, c, v): (&Option<Position>, &Option<Sprite>, &Option<Visibility>)| {
-            Some((p.as_ref()?, c.as_ref()?, v.as_ref()?))
-        },
-    );
-    for (p, c, v) in drawable {
-        if v.0 {
-            // Handle special case for '.' only draw if nothing else present
-            if c.0 == '.' && output[p.0.y][p.0.x] != '?' {
-                // Do nothing, '.' can be in background
-            } else {
-                output[p.0.y][p.0.x] = c.0;
+    {
+        let it = world.filter(vec![
+            ComponentType::Position,
+            ComponentType::Visibility,
+            ComponentType::Sprite,
+        ]);
+        for a in it.archetypes {
+            let pos = a.borrow_component_vec::<Position>().unwrap();
+            let vis = a.borrow_component_vec::<Visibility>().unwrap();
+            let spr = a.borrow_component_vec::<Sprite>().unwrap();
+
+            for i in 0..a.length {
+                // TODO: remove after fully implemented archetypes
+                match (pos[i].as_ref(), vis[i].as_ref(), spr[i].as_ref()) {
+                    (Some(p), Some(v), Some(s)) => {
+                        if v.0 {
+                            // Handle special case for '.' only draw if nothing else present
+                            if s.0 == '.' && output[p.0.y][p.0.x].c != '?' {
+                                // Do nothing, '.' can be in background
+                            } else {
+                                output[p.0.y][p.0.x].c = s.0;
+                            }
+                        }
+                    }
+                    _ => {}
+                }
             }
         }
     }
 
-    let mut highlights = world.borrow_mut_component_vec::<BackgroundHighlight>();
-    for y in 0..world.height {
-        output.push(Vec::new());
-        for x in 0..world.width {
-            let id = world.get_entity(Point { x: x, y: y });
-            if id.is_some()
-                && highlights.as_ref().is_some()
-                && highlights.as_ref().unwrap()[id.unwrap()].as_ref().is_some()
-            {
-                let color = highlights.as_ref().unwrap()[id.unwrap()]
-                    .as_ref()
-                    .unwrap()
-                    .0;
-                match execute!(stdout(), SetBackgroundColor(color)) {
+    let height = world.height;
+    let width = world.width;
+    let it = world.filter(vec![ComponentType::BackgroundHighlight]);
+    for a in it.archetypes {
+        // TODO: remove after fully implemented archetypes
+        let mut hig = match a.borrow_mut_component_vec::<BackgroundHighlight>() {
+            Some(a) => a,
+            _ => continue, // no highlights to populate
+        };
+        let pos = a.borrow_component_vec::<Position>().unwrap();
+
+        for i in 0..a.length {
+            // TODO: remove after fully implemented archetypes
+            match (pos[i].as_ref(), hig[i].as_ref()) {
+                (Some(p), Some(bg)) => {
+                    output[p.0.y][p.0.x].bg = Some(bg.0);
+                    hig[i] = None;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    for y in 0..height {
+        for x in 0..width {
+            let color = output[y][x].bg;
+            if color.is_some() {
+                match execute!(stdout(), SetBackgroundColor(color.unwrap())) {
                     Err(_) => panic!("error setting background color"),
                     _ => {}
                 };
-                highlights.as_mut().unwrap()[id.unwrap()] = None;
             }
-
-            print!("{}", output[y][x]);
+            print!("{}", output[y][x].c);
             match execute!(stdout(), ResetColor) {
                 Err(_) => panic!("error reseting background color"),
                 _ => {}
@@ -105,8 +134,8 @@ fn render_system_update(world: &World) {
         }
         println!("");
     }
-    println!("");
 
+    println!("");
     // execute!(stdout(), MoveUp(10)).unwrap();
 }
 
