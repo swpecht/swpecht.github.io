0a6ce747a473e1e4922bbf77874197553b7cc33e
Steven Pecht <steven.pecht@gmail.com>
2022-02-12 15:19:17 -0800

    Add benchmarks

---
+++ projects/wordle_bot/benches/eval_benchmark.rs
@@ -0,0 +1,18 @@
+use std::collections::HashSet;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use wordle_bot::{evaluate_guess, load_word_list};
+
+const ANSWER_FILE: &str = "data/wordle-answers-alphabetical.txt";
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut answers = HashSet::new();
+    load_word_list(ANSWER_FILE, &mut answers);
+
+    c.bench_function("eval crane", |b| {
+        b.iter(|| evaluate_guess(black_box("crane"), &answers))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
