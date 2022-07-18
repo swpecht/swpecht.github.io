fd405e777cd0878e7a5da73600b85d40cb3cf63d
Steven Pecht <steven.pecht@gmail.com>
2022-07-17 20:55:03 -0700

    Add P2 support to minimax

--- projects/liars_poker_bot/src/minimax_agent.rs
+++ projects/liars_poker_bot/src/minimax_agent.rs
@@ -3,7 +3,10 @@ use log::debug;
 use crate::{
     agents::Agent,
     game_tree::GameTree,
-    liars_poker::{apply_action, get_winner, DiceState, GameState, Player, DICE_SIDES, NUM_DICE},
+    liars_poker::{
+        apply_action, get_acting_player, get_winner, DiceState, GameState, Player, DICE_SIDES,
+        NUM_DICE,
+    },
 };
 
 pub struct MinimaxAgent {
@@ -20,19 +23,29 @@ impl Agent for MinimaxAgent {
         g: &crate::liars_poker::GameState,
         possible_moves: &Vec<crate::liars_poker::Action>,
     ) -> crate::liars_poker::Action {
-        let mut cur_max = f32::MIN;
+        let acting_player = get_acting_player(g);
+
+        let mut cur_max = match acting_player {
+            Player::P1 => f32::MIN,
+            Player::P2 => f32::MAX,
+        };
         let mut cur_move = None;
         for a in possible_moves {
             let f = apply_action(g, a);
             debug!("Evaluating: {:?}", f);
-            // let value = minimax(g, &mut f32::MIN, &mut f32::MAX, true);
             let t = GameTree::new(&f);
             // print!("{:?}", t);
 
             let value = t.get(0).score.unwrap();
 
             debug!("value: {:?}", value);
-            if value > cur_max {
+
+            let is_better = match acting_player {
+                Player::P1 => value > cur_max,
+                Player::P2 => value < cur_max,
+            };
+
+            if is_better {
                 cur_max = value;
                 cur_move = Some(a)
             }
