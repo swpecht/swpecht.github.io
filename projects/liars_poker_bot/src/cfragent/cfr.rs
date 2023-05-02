2ee9db2d5a0d96f9eec67fc6de9d12323a8657cb
Steven <steven.pecht@gmail.com>
2023-05-02 19:37:53 +0200

    Add logging for nodes touched to vanilla CFR

--- projects/liars_poker_bot/src/cfragent/cfr.rs
+++ projects/liars_poker_bot/src/cfragent/cfr.rs
@@ -1,6 +1,6 @@
 use std::{cell::RefCell, rc::Rc};
 
-use log::trace;
+use log::{debug, trace};
 
 use crate::{
     cfragent::cfrnode::CFRNode,
@@ -59,6 +59,9 @@ impl VanillaCFR {
         chance_reach: f64,
         mut phase: CFRPhase,
     ) -> f64 {
+        if self.nodes_touched % 1000000 == 0 {
+            debug!("cfr touched {} nodes", self.nodes_touched);
+        }
         self.nodes_touched += 1;
 
         let cur_player = gs.cur_player();
