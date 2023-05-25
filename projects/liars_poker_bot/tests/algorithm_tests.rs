e372bf5cbf7f6591bab81d1880367480e8e8b720
Steven <steven.pecht@gmail.com>
2023-05-25 11:31:36 +0200

    Add integration test to verify open hand solver caching for euchre

--- projects/liars_poker_bot/tests/algorithm_tests.rs
+++ projects/liars_poker_bot/tests/algorithm_tests.rs
@@ -2,13 +2,16 @@ use approx::assert_relative_eq;
 use liars_poker_bot::{
     algorithms::{
         exploitability::exploitability,
-        ismcts::{ISMCTBotConfig, ISMCTSBot, ISMCTSFinalPolicyType, RandomRolloutEvaluator},
+        ismcts::{
+            Evaluator, ISMCTBotConfig, ISMCTSBot, ISMCTSFinalPolicyType, RandomRolloutEvaluator,
+        },
+        open_hand_solver::OpenHandSolver,
     },
     cfragent::{CFRAgent, CFRAlgorithm},
     database::memory_node_store::MemoryNodeStore,
-    game::kuhn_poker::KuhnPoker,
+    game::{euchre::Euchre, kuhn_poker::KuhnPoker, GameState},
 };
-use rand::SeedableRng;
+use rand::{rngs::StdRng, seq::SliceRandom, SeedableRng};
 
 #[test]
 fn test_ismcts_exploitability() {
@@ -37,4 +40,32 @@ fn test_cfr_exploitability() {
 
     let exploitability = exploitability(KuhnPoker::game(), &mut agent.ns).nash_conv;
     assert_relative_eq!(exploitability, 0.0, epsilon = 0.001);
+}
+
+#[test]
+fn test_open_hand_solver_euchre() {
+    let mut rng: StdRng = SeedableRng::seed_from_u64(51);
+    let mut actions = Vec::new();
+
+    let mut cached = OpenHandSolver::new(100, rng.clone());
+    let mut no_cache = OpenHandSolver::new_without_cache(100, rng.clone());
+
+    for _ in 0..100 {
+        let mut gs = Euchre::new_state();
+        while gs.is_chance_node() {
+            gs.legal_actions(&mut actions);
+            let a = actions.choose(&mut rng).unwrap();
+            gs.apply_action(*a);
+        }
+
+        while !gs.is_terminal() {
+            let c = cached.evaluate(&gs);
+            let no_c = no_cache.evaluate(&gs);
+            assert_eq!(c, no_c);
+
+            gs.legal_actions(&mut actions);
+            let a = actions.choose(&mut rng).unwrap();
+            gs.apply_action(*a);
+        }
+    }
 }
