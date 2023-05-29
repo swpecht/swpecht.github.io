1d516bad447d82afebcb397415d44401bd248e4e
Steven <steven.pecht@gmail.com>
2023-05-29 11:54:20 +0200

    Change solver benchmark to run on different games

--- projects/liars_poker_bot/benches/open_hand_solver_benchmarks.rs
+++ projects/liars_poker_bot/benches/open_hand_solver_benchmarks.rs
@@ -1,24 +1,36 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion};
 use liars_poker_bot::{
     actions,
     algorithms::open_hand_solver::OpenHandSolver,
-    game::{euchre::Euchre, GameState},
+    game::{
+        euchre::{Euchre, EuchreGameState},
+        GameState,
+    },
 };
 use rand::{rngs::StdRng, seq::SliceRandom, SeedableRng};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut rng: StdRng = SeedableRng::seed_from_u64(42);
-    let mut evaluator = OpenHandSolver::new(100, rng.clone());
+    let mut evaluator = OpenHandSolver::new(100, SeedableRng::seed_from_u64(100));
 
+    c.bench_function("open hand evaluator 100", |b| {
+        b.iter(|| evaluate_games(&mut evaluator, &mut rng))
+    });
+}
+
+fn evaluate_games(evaluator: &mut OpenHandSolver<EuchreGameState>, rng: &mut StdRng) {
+    let gs = &get_game(rng);
+    evaluator.evaluate_player(gs, 3);
+}
+
+fn get_game(rng: &mut StdRng) -> EuchreGameState {
     let mut gs = Euchre::new_state();
     while gs.is_chance_node() {
-        let a = *actions!(gs).choose(&mut rng).unwrap();
+        let a = *actions!(gs).choose(rng).unwrap();
         gs.apply_action(a)
     }
 
-    c.bench_function("open hand evaluator 100", |b| {
-        b.iter(|| evaluator.evaluate_player(black_box(&gs), 3))
-    });
+    gs
 }
 
 criterion_group!(benches, criterion_benchmark);
