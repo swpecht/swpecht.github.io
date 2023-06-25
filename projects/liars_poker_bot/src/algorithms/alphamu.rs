72c9f074788d7f0aaaf7be8e35b9d23cf70ef39d
Steven <steven.pecht@gmail.com>
2023-06-25 18:10:46 +0200

    Remove debug logging for front sizes

--- projects/liars_poker_bot/src/algorithms/alphamu.rs
+++ projects/liars_poker_bot/src/algorithms/alphamu.rs
@@ -253,7 +253,7 @@ impl<G: GameState + ResampleFromInfoState, E: Evaluator<G>> AlphaMuBot<G, E> {
                 }
 
                 self.update_useful_worlds(&front, &mut worlds);
-                debug!("iterating on min nodes, front size: {}: {}", m, front.len());
+                // debug!("iterating on min nodes, front size: {}: {}", m, front.len());
 
                 // set everything in the front to be useless
                 for (i, _) in worlds.iter().enumerate().filter(|(_, w)| w.is_useless()) {
