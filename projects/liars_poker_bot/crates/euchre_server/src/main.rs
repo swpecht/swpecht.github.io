e0b941c79978b72aff0561c42e4d4778ce9846b3
Steven <steven.pecht@gmail.com>
2023-08-06 09:00:21 +0800

    Change actix binding to 0.0.0.0

--- projects/liars_poker_bot/crates/euchre_server/src/main.rs
+++ projects/liars_poker_bot/crates/euchre_server/src/main.rs
@@ -291,7 +291,7 @@ async fn main() -> std::io::Result<()> {
             .service(actix_files::Files::new("/", "./static").index_file("index.html"))
             .default_service(web::get().to(not_found))
     })
-    .bind(("127.0.0.1", 4000))?
+    .bind(("0.0.0.0", 4000))?
     .run()
     .await
 }
