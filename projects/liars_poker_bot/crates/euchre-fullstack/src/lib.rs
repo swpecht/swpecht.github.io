49b075d06af3162dae0a5cd089d771e64921a818
Steven <steven.pecht@gmail.com>
2023-08-30 09:19:23 +1000

    Remove full stack experiment

--- projects/liars_poker_bot/crates/euchre-fullstack/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub const ACTION_BUTTON_CLASS: &str = "bg-white outline outline-black hover:bg-slate-100 focus:outline-none focus:ring focus:bg-slate-100 active:bg-slate-200 rounded-lg disabled:outline-white";
-
-pub struct PlayerId {
-    pub id: usize,
-}
-
-pub mod in_game;
