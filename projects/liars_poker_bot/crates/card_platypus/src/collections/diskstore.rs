6e55272fbcd961f139adf2056156a682d5f7dae2
Steven <steven.pecht@gmail.com>
2023-09-14 11:05:02 +1200

    Remove commit from len call

--- projects/liars_poker_bot/crates/card_platypus/src/collections/diskstore.rs
+++ projects/liars_poker_bot/crates/card_platypus/src/collections/diskstore.rs
@@ -1,21 +1,22 @@
-use std::{fs, path::Path};
+use std::{env, fs, path::Path};
 
 use dashmap::{
     mapref::one::{Ref, RefMut},
     DashMap,
 };
-use heed::{types::SerdeBincode, Database, Env, EnvOpenOptions};
+use heed::{flags::Flags, types::SerdeBincode, Database, Env, EnvOpenOptions};
 use itertools::Itertools;
 
 use crate::{cfragent::cfres::InfoState, istate::IStateKey};
 
-const MAX_CACHE_LEN: usize = 50_000_000;
+const DEFAULT_CACHE_LEN: usize = 30_000_000;
 const MAX_DB_SIZE_GB: usize = 200;
 
 pub struct DiskStore {
     env: Option<Env>,
     db: Option<Database<heed::types::ByteSlice, SerdeBincode<InfoState>>>,
     cache: DashMap<IStateKey, InfoState>,
+    max_cache_len: usize,
 }
 
 impl DiskStore {
@@ -23,20 +24,42 @@ impl DiskStore {
         let cache = DashMap::new();
         if let Some(path) = path {
             fs::create_dir_all(path).unwrap();
-            let env = EnvOpenOptions::new()
-                .map_size(MAX_DB_SIZE_GB * 1024 * 1024 * 1024)
-                .open(path)?;
-            let db = env.create_database(None)?;
+
+            let mut env_builder = EnvOpenOptions::new();
+            unsafe {
+                // Only sync meta data at the end of a transaction, this can hurt the durability
+                // of the database, but cannot lead to corruption
+                env_builder.flag(Flags::MdbNoMetaSync);
+                // Disable OS read-ahead, can improve perf when db is larger than RAM
+                env_builder.flag(Flags::MdbNoRdAhead);
+                // Improves write performance, but can cause corruption if there is a bug in application
+                // code that overwrite the memory address
+                env_builder.flag(Flags::MdbWriteMap);
+                // Avoid zeroing memory before use -- can cause issues with
+                // sensitive data, but not a risk here.
+                env_builder.flag(Flags::MdbNoMemInit);
+                // Uses asynchronous flushes to disk, a system crash can corrupt the databse or lose data
+                // env_builder.flag(Flags::MdbMapAsync);
+                // todo, explore other flags
+            }
+            env_builder.map_size(MAX_DB_SIZE_GB * 1024 * 1024 * 1024);
+
+            let env = env_builder.open(path)?;
+            // need to open rather than create for WriteMap to work
+            let db = env.open_database(None).unwrap().unwrap();
+            // let db = env.create_database(None).unwrap();
             Ok(Self {
                 env: Some(env),
                 db: Some(db),
                 cache,
+                max_cache_len: DEFAULT_CACHE_LEN,
             })
         } else {
             Ok(Self {
                 env: None,
                 db: None,
                 cache,
+                max_cache_len: DEFAULT_CACHE_LEN,
             })
         }
     }
@@ -79,7 +102,7 @@ impl DiskStore {
     pub fn put(&self, key: IStateKey, value: InfoState) {
         self.cache.insert(key, value);
 
-        if self.cache.len() > MAX_CACHE_LEN {
+        if self.cache.len() > self.max_cache_len {
             self.commit();
         }
     }
@@ -114,12 +137,11 @@ impl DiskStore {
         self.db.unwrap().get(&rtxn, k.as_slice()).unwrap()
     }
 
-    /// Does a commit of the cache, could be expensive
+    /// Returns the len of commited items, doesn't include item in the cache
     pub fn len(&self) -> usize {
         if self.db.is_none() || self.env.is_none() {
             self.cache.len()
         } else {
-            self.commit();
             let rtxn = self.env.as_ref().unwrap().read_txn().unwrap();
             self.db.unwrap().len(&rtxn).unwrap() as usize
         }
@@ -128,5 +150,22 @@ impl DiskStore {
     #[must_use]
     pub fn is_empty(&self) -> bool {
         self.len() == 0
+    }
+
+    pub fn set_cache_len(&mut self, len: usize) {
+        self.max_cache_len = len;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use super::DiskStore;
+
+    #[test]
+    fn test_open_database() {
+        let path = Path::new("/tmp/card_platypus").join("bytemuck.mdb");
+        let mut x = DiskStore::new(Some(&path)).unwrap();
     }
 }
