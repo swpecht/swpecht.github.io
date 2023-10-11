e6174d220fe1160d9f8ea5de56ed7f17cc918781
Steven <steven.pecht@gmail.com>
2023-10-11 17:24:31 +0900

    Add basic scrolling

--- projects/racoon/src/input.rs
+++ projects/racoon/src/input.rs
@@ -1,13 +1,14 @@
-use bevy::{prelude::*, window::PrimaryWindow};
+use bevy::{input::mouse::MouseWheel, prelude::*, window::PrimaryWindow};
 
-const CAMERA_SPEED: f32 = 2.0;
+const CAMERA_PAN_SPEED: f32 = 2.0;
+const CAMERA_ZOOM_SPEED: f32 = 0.1;
 
 pub struct CursorPlugin {}
 
 impl Plugin for CursorPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, input_setup)
-            .add_systems(Update, (cursor_system, camera_system));
+            .add_systems(Update, (cursor_system, camera_pan, camera_zoom));
     }
 }
 
@@ -56,24 +57,48 @@ fn cursor_system(
     }
 }
 
-fn camera_system(
+fn camera_pan(
     mut q_camera: Query<(&Camera, &mut Transform), With<MainCamera>>,
     keys: Res<Input<KeyCode>>,
 ) {
     let (camera, mut camera_transform) = q_camera.single_mut();
     if keys.pressed(KeyCode::W) {
-        camera_transform.translation += Vec3::Y * CAMERA_SPEED;
+        camera_transform.translation += Vec3::Y * CAMERA_PAN_SPEED;
     }
 
     if keys.pressed(KeyCode::A) {
-        camera_transform.translation -= Vec3::X * CAMERA_SPEED;
+        camera_transform.translation -= Vec3::X * CAMERA_PAN_SPEED;
     }
 
     if keys.pressed(KeyCode::S) {
-        camera_transform.translation -= Vec3::Y * CAMERA_SPEED;
+        camera_transform.translation -= Vec3::Y * CAMERA_PAN_SPEED;
     }
 
     if keys.pressed(KeyCode::D) {
-        camera_transform.translation += Vec3::X * CAMERA_SPEED;
+        camera_transform.translation += Vec3::X * CAMERA_PAN_SPEED;
+    }
+}
+
+fn camera_zoom(
+    mut query: Query<&mut OrthographicProjection, With<Camera>>,
+    mut scroll_evr: EventReader<MouseWheel>,
+) {
+    let mut scroll_amount = 0.;
+    use bevy::input::mouse::MouseScrollUnit;
+    for ev in scroll_evr.iter() {
+        match ev.unit {
+            MouseScrollUnit::Line => {
+                scroll_amount += ev.y;
+            }
+            MouseScrollUnit::Pixel => {
+                scroll_amount += ev.y;
+            }
+        }
+    }
+
+    for mut projection in query.iter_mut() {
+        let mut log_scale = projection.scale.ln();
+        log_scale -= CAMERA_ZOOM_SPEED * scroll_amount;
+        projection.scale = log_scale.exp();
     }
 }
