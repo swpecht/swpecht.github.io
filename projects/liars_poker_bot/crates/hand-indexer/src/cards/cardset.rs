6dea8a5dbaa6592d95d8b5d3ff4a586e9322620b
Steven <steven.pecht@gmail.com>
2023-12-10 17:28:12 +0900

    Change to incrementing card index directly

---
+++ projects/liars_poker_bot/crates/hand-indexer/src/cards/cardset.rs
@@ -0,0 +1,110 @@
+use std::fmt::Debug;
+
+use super::Card;
+
+#[derive(Clone, Copy, Default)]
+pub struct CardSet(pub(super) u64);
+
+impl CardSet {
+    /// Create a CardSet with all cards
+    pub fn all() -> Self {
+        Self(!0)
+    }
+
+    pub fn pop_highest(&mut self) -> Option<Card> {
+        let highest = self.highest()?;
+        self.remove(highest);
+        Some(highest)
+    }
+
+    pub fn pop_lowest(&mut self) -> Option<Card> {
+        let lowest = self.lowest()?;
+        self.remove(lowest);
+        Some(lowest)
+    }
+
+    pub fn insert(&mut self, c: Card) {
+        self.0 |= c.0;
+    }
+
+    pub fn insert_all(&mut self, set: CardSet) {
+        self.0 |= set.0;
+    }
+
+    pub fn highest(&self) -> Option<Card> {
+        if self.is_empty() {
+            return None;
+        }
+
+        let rank = 64 - self.0.leading_zeros() - 1;
+        Some(Card(1 << rank))
+    }
+
+    pub fn lowest(&self) -> Option<Card> {
+        if self.is_empty() {
+            return None;
+        }
+
+        let rank = self.0.trailing_zeros();
+        Some(Card(1 << rank))
+    }
+
+    pub fn increment_highest(&self) -> Option<CardSet> {
+        let mut new = *self;
+        let highest_mask = new.highest()?.0;
+        new.0 &= !highest_mask;
+        new.0 |= highest_mask.checked_shl(1)?;
+        Some(new)
+    }
+
+    pub fn remove(&mut self, card: Card) {
+        self.0 &= !card.0;
+    }
+
+    pub fn remove_all(&mut self, set: CardSet) {
+        self.0 &= !set.0;
+    }
+
+    pub fn constains_all(&self, set: CardSet) -> bool {
+        self.0 | set.0 == self.0
+    }
+
+    pub fn contains(&self, card: Card) -> bool {
+        self.0 & card.0 > 0
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.0 == 0
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.count_ones() as usize
+    }
+}
+
+impl Debug for CardSet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{:#b}", self.0))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_cardset() {
+        let mut set = CardSet::default();
+        set.insert(Card::new(1));
+        set.insert(Card::new(5));
+
+        set = set.increment_highest().unwrap();
+        assert_eq!(set.highest().unwrap(), Card::new(6));
+        assert!(set.contains(Card::new(1)));
+
+        assert_eq!(set.lowest().unwrap(), Card::new(1));
+        assert_eq!(set.pop_lowest().unwrap(), Card::new(1));
+        assert_eq!(set.lowest().unwrap(), Card::new(6));
+    }
+}
--- projects/liars_poker_bot/crates/hand-indexer/src/cards/mod.rs
+++ projects/liars_poker_bot/crates/hand-indexer/src/cards/mod.rs
@@ -1,12 +1,16 @@
 use itertools::{Combinations, Itertools};
 use std::fmt::Debug;
 
+use self::cardset::CardSet;
+
 const SPADES: u64 = 0b1111111111111;
 const CLUBS: u64 = SPADES << 13;
 const HEARTS: u64 = CLUBS << 13;
 const DIAMONDS: u64 = HEARTS << 13;
 
 const MAX_CARDS: usize = 64;
+
+pub mod cardset;
 
 /// Represents a single card
 ///
@@ -25,42 +29,6 @@ impl Card {
 }
 
 impl Debug for Card {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_fmt(format_args!("{:#b}", self.0))
-    }
-}
-
-#[derive(Clone, Copy, Default)]
-pub struct CardSet(u64);
-
-impl CardSet {
-    pub fn insert(&mut self, c: Card) {
-        self.0 |= c.0;
-    }
-
-    pub fn highest(&self) -> Card {
-        let rank = 64 - self.0.leading_zeros() - 1;
-        Card(1 << rank)
-    }
-
-    pub fn remove(&mut self, card: Card) {
-        self.0 &= !card.0;
-    }
-
-    pub fn remove_all(&mut self, set: CardSet) {
-        self.0 &= !set.0;
-    }
-
-    pub fn constains_all(&self, set: CardSet) -> bool {
-        self.0 | set.0 == self.0
-    }
-
-    pub fn contains(&self, card: Card) -> bool {
-        self.0 & card.0 > 0
-    }
-}
-
-impl Debug for CardSet {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("{:#b}", self.0))
     }
@@ -120,8 +88,7 @@ impl Deck {
     /// Returns the lowest rank card in the deck by representation, this
     /// does not necessarily correspond to a cards value in a given game
     fn lowest(&self) -> Card {
-        let rank = self.remaining_cards.0.trailing_zeros();
-        Card(1 << rank)
+        self.remaining_cards.lowest().unwrap()
     }
 
     fn pop(&mut self) -> Option<Card> {
@@ -175,27 +142,27 @@ impl Iterator for DeckIterator {
 /// Want to store an array or iterators for the different combinations of cards
 struct DealEnumerationIterator<const R: usize> {
     cards_per_round: [usize; R],
-    /// the index for the next card set
-    /// these are the counter variables if generating in a loop
-    next_candidate_idx: [Vec<usize>; R],
-    last_set: Option<[CardSet; R]>,
+    next_candidate_set: Option<[CardSet; R]>,
     deck: Deck,
 }
 
 impl<const R: usize> DealEnumerationIterator<R> {
     pub fn new(deck: Deck, cards_per_round: [usize; R]) -> Self {
-        let mut first_index = std::array::from_fn(|_| Vec::new());
+        let mut first_candidate_set = [CardSet::default(); R];
+        let mut i = 0;
         for r in 0..R {
-            for i in 0..cards_per_round[r] {
-                first_index[r].push(i);
+            for _ in 0..cards_per_round[r] {
+                first_candidate_set[r].insert(Card::new(i));
+                i += 1;
             }
         }
+
+        assert!(is_valid(first_candidate_set, CardSet::all()));
 
         Self {
             cards_per_round,
             deck,
-            next_candidate_idx: first_index,
-            last_set: None,
+            next_candidate_set: Some(first_candidate_set),
         }
     }
 }
@@ -204,122 +171,101 @@ impl<const R: usize> Iterator for DealEnumerationIterator<R> {
     type Item = [CardSet; R];
 
     fn next(&mut self) -> Option<Self::Item> {
-        // todo: handle the first iteration
-        let valid_cards;
-        if let Some(ls) = self.last_set {
-            valid_cards = round_valid_cards(ls, self.deck.remaining_cards);
-        } else {
-            let mut next_set = [CardSet::default(); R];
-            for r in 0..R {
-                let valid_cards = round_valid_cards(next_set, self.deck.remaining_cards);
-                (next_set[r], self.next_candidate_idx[r]) =
-                    find_next_cardset(self.next_candidate_idx[r].clone(), valid_cards[r])?;
-            }
+        let mut candidate = self.next_candidate_set?;
 
-            self.last_set = Some(next_set);
-
-            return self.last_set;
+        while !is_valid(candidate, self.deck.remaining_cards) {
+            candidate = incremenet_deal(candidate, self.deck.remaining_cards)?;
         }
 
-        let mut next_set = self.last_set.unwrap();
-        let mut updated_round = R;
-        for r in (0..R).rev() {
-            if let Some((round_set, next_rnd_idx)) =
-                find_next_cardset(self.next_candidate_idx[r].clone(), valid_cards[r])
-            {
-                next_set[r] = round_set;
-                self.next_candidate_idx[r] = next_rnd_idx;
-                updated_round = r;
-                break;
-            } else if r == 0 {
-                // if we fail to find an increment for round 0, we're at the end of the iterator
-                return None;
-            }
-        }
-
-        // fill the forward rounds with the lowest index and values
-        for r in (updated_round + 1)..R {
-            let valid_cards = round_valid_cards(next_set, self.deck.remaining_cards);
-            let candidate_index = (0..self.cards_per_round[r]).collect_vec();
-            (next_set[r], self.next_candidate_idx[r]) =
-                find_next_cardset(candidate_index, valid_cards[r])?;
-        }
-
-        self.last_set = Some(next_set);
-
-        self.last_set
+        let cur_set = candidate;
+        self.next_candidate_set = incremenet_deal(candidate, self.deck.remaining_cards);
+        Some(cur_set)
     }
-}
-
-fn find_next_cardset(
-    mut candidate_index: Vec<usize>,
-    valid_cards: CardSet,
-) -> Option<(CardSet, Vec<usize>)> {
-    let set;
-
-    loop {
-        if let Some(found_set) = index_to_card_set(&candidate_index, &valid_cards) {
-            set = found_set;
-            break;
-        }
-        candidate_index = increment_index_round(candidate_index)?;
-    }
-
-    let next_candidate_index = increment_index_round(candidate_index).unwrap();
-    Some((set, next_candidate_index))
 }
 
 /// Returns the valid possible cards for each round
-fn round_valid_cards<const R: usize>(set: [CardSet; R], starting_valid: CardSet) -> [CardSet; R] {
+fn round_valid_cards<const R: usize>(deal: [CardSet; R], starting_valid: CardSet) -> [CardSet; R] {
     let mut round_valid_cards = [CardSet::default(); R];
     round_valid_cards[0] = starting_valid;
     for r in 1..R {
         round_valid_cards[r] = round_valid_cards[r - 1];
-        round_valid_cards[r].remove_all(set[r - 1]);
+        round_valid_cards[r].remove_all(deal[r - 1]);
     }
     round_valid_cards
 }
 
-/// Increments the set index, "carrying" when the last digit gets to
-/// MAX_CARDS
-fn increment_index_round(index: Vec<usize>) -> Option<Vec<usize>> {
-    // TODO: how do we allow this to wrap for later rounds? -- start at 0 each time we go through a new round?
-    increment_index_round_r(index, MAX_CARDS)
+fn is_valid<const R: usize>(deal: [CardSet; R], valid_cards: CardSet) -> bool {
+    let mut check_set = CardSet::default();
+    for set in deal {
+        check_set.insert_all(set);
+    }
+
+    let each_round_different_cards = deal.iter().map(|x| x.len()).sum::<usize>() == check_set.len();
+    let each_card_valid = deal.into_iter().all(|x| valid_cards.constains_all(x));
+
+    each_round_different_cards && each_card_valid
 }
 
-fn increment_index_round_r(mut index: Vec<usize>, max_rank: usize) -> Option<Vec<usize>> {
-    let last = index.last()?;
+/// Increments the set index, "carrying" when the last digit gets to
+/// MAX_CARDS
+fn increment_cardset(set: CardSet) -> Option<CardSet> {
+    // TODO: how do we allow this to wrap for later rounds? -- start at 0 each time we go through a new round?
+    increment_cardset_r(set, MAX_CARDS)
+}
+
+fn increment_cardset_r(mut set: CardSet, max_rank: usize) -> Option<CardSet> {
+    let last = set.highest()?.rank();
     // handle the simple case where no carrying occurs
     if last + 1 < max_rank {
-        *index.last_mut()? += 1;
-        return Some(index);
+        set = set.increment_highest()?;
+        return Some(set);
     }
 
     // recursively do all the carrying for the base index
-    index.pop();
-    let mut index = increment_index_round_r(index, max_rank - 1)?;
+    set.pop_highest();
+    set = increment_cardset_r(set, max_rank - 1)?;
 
-    if index.last()? + 1 < max_rank {
-        index.push(index.last()? + 1);
-        Some(index)
+    if set.highest()?.rank() + 1 < max_rank {
+        set.insert(Card::new(set.highest()?.rank() + 1));
+        Some(set)
     } else {
         // no further indexes are possible
         None
     }
 }
 
-fn index_to_card_set(index: &[usize], valid_cards: &CardSet) -> Option<CardSet> {
-    let mut set = CardSet::default();
-
-    for c in index.iter().map(|x| Card::new(*x)) {
-        set.insert(c)
+fn incremenet_deal<const R: usize>(
+    mut deal: [CardSet; R],
+    deck_cards: CardSet,
+) -> Option<[CardSet; R]> {
+    let mut cards_per_round = [0; R];
+    for r in 0..R {
+        cards_per_round[r] = deal[r].len();
     }
 
-    if !valid_cards.constains_all(set) {
-        return None;
+    let mut updated_round = R;
+    for r in (0..R).rev() {
+        if let Some(set) = increment_cardset(deal[r]) {
+            deal[r] = set;
+            updated_round = r;
+            break;
+        } else if r == 0 {
+            // if we fail to find an increment for round 0, we're at the end of the iterator
+            return None;
+        }
     }
 
-    Some(set)
+    // fill the forward rounds with the lowest index and values
+    for r in (updated_round + 1)..R {
+        let mut valid_cards = round_valid_cards(deal, deck_cards);
+        let mut new_set = CardSet::default();
+        for _ in 0..cards_per_round[r] {
+            new_set.insert(valid_cards[r].pop_lowest().unwrap())
+        }
+        deal[r] = new_set;
+    }
+
+    Some(deal)
 }
 
 #[cfg(test)]
@@ -338,11 +284,11 @@ mod tests {
 
         let mut set = CardSet::default();
         set.insert(Card(0b10));
-        assert_eq!(set.highest(), Card(0b10));
+        assert_eq!(set.highest().unwrap(), Card(0b10));
         set.insert(Card(0b100));
-        assert_eq!(set.highest(), Card(0b100));
+        assert_eq!(set.highest().unwrap(), Card(0b100));
         set.insert(Card(0b1));
-        assert_eq!(set.highest(), Card(0b100));
+        assert_eq!(set.highest().unwrap(), Card(0b100));
     }
 
     #[test]
@@ -353,8 +299,8 @@ mod tests {
         assert_eq!(count_combinations([2]), 1326);
 
         assert_eq!(count_combinations([2, 2]), 1_624_350);
-        // // Flop: 52 choose 2 * 50 choose 3
-        // assert_eq!(count_combinations([2, 3]), 25_989_600);
+        // Flop: 52 choose 2 * 50 choose 3
+        assert_eq!(count_combinations([2, 3]), 25_989_600);
         // // Turn: 52 choose 2 * 50 choose 3 * 47
         // assert_eq!(count_combinations([2, 3, 1]), 1_221_511_200);
         // // River: 52 choose 2 * 50 choose 3 * 47 * 46
