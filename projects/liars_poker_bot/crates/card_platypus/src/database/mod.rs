926a3a1237ef2e32251371d923f07b21b53bccb9
Steven <steven.pecht@gmail.com>
2024-01-15 15:45:57 +0700

    Remove dead code

--- projects/liars_poker_bot/crates/card_platypus/src/database/mod.rs
+++ projects/liars_poker_bot/crates/card_platypus/src/database/mod.rs
@@ -75,18 +75,6 @@ pub struct NodeStore {
 impl NodeStore {
     /// len is the number of infostates to provision for
     pub fn new_euchre(path: Option<&Path>) -> anyhow::Result<Self> {
-        // let (phf, n) = generate_euchre_phf().context("failed to generate phf")?;
-
-        // let phf = match load_phf(path) {
-        //     Ok(x) => x,
-        //     Err(x) => {
-        //         if path.is_some() {
-        //             warn!("failed to load index: {}", x);
-        //         }
-        //         HashStore::default()
-        //     }
-        // };
-
         // TODO: in the future can use make it so the hashing happens in stages so that later istates are offset from others as a way to save space
         // Or can pass in the max num cards as a parameter
         let istate_iter = EuchreIsomorphicIStateIterator::new(4);
@@ -131,7 +119,6 @@ impl NodeStore {
 
     pub fn get(&self, key: &IStateKey) -> Option<InfoState> {
         let index: usize = self.phf.hash(key) as usize;
-        // let index: usize = self.phf.get_hash(key)?;
         let start = index * BUCKET_SIZE;
 
         if start + BUCKET_SIZE > self.mmap.len() {
@@ -146,16 +133,11 @@ impl NodeStore {
         }
 
         let info = bytemuck::cast_slice::<u8, InfoState>(data)[0];
-        // let info = match rmp_serde::from_slice(data) {
-        //     Ok(x) => x,
-        //     Err(_) => return None,
-        // };
         Some(info)
     }
 
     pub fn put(&mut self, key: &IStateKey, value: &InfoState) {
         let index: usize = self.phf.hash(key) as usize;
-        // let index: usize = self.phf.hash(key);
         let start = index * BUCKET_SIZE;
 
         if start + BUCKET_SIZE > self.mmap.len() {
@@ -229,49 +211,4 @@ fn load_phf(path: Option<&Path>) -> anyhow::Result<HashStore> {
     } else {
         bail!("path is none")
     }
-}
-
-fn generate_phf<T: GameState>(new_state: fn() -> T) -> anyhow::Result<(Mphf<IStateKey>, usize)> {
-    let mut istates = HashSet::new();
-
-    let mut gs = (new_state)();
-    populate_all_states(&mut istates, &mut gs);
-
-    let n = istates.len();
-    let phf = Mphf::new_parallel(1.7, &istates.iter().copied().collect_vec(), None);
-    validate_phf(&istates, &phf);
-
-    Ok((phf, n))
-}
-
-fn populate_all_states<T: GameState>(istates: &mut HashSet<IStateKey>, gs: &mut T) {
-    if gs.is_terminal() {
-        return;
-    }
-
-    if !gs.is_chance_node() {
-        let key = gs.istate_key(gs.cur_player());
-        istates.insert(key);
-    }
-
-    let actions = actions!(gs);
-    for a in actions {
-        gs.apply_action(a);
-        populate_all_states(istates, gs);
-        gs.undo()
-    }
-}
-
-fn validate_phf(istates: &HashSet<IStateKey>, phf: &Mphf<IStateKey>) {
-    let n = istates.len();
-    // Get hash value of all objects
-    let mut hashes = Vec::new();
-    for v in istates {
-        hashes.push(phf.hash(v));
-    }
-    hashes.sort();
-
-    // Expected hash output is set of all integers from 0..n
-    let expected_hashes: Vec<u64> = (0..n as u64).collect();
-    assert!(hashes == expected_hashes);
 }
