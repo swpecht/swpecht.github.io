b7a7253835b76d4f3eda205ac7206a8b66614877
Steven <steven.pecht@gmail.com>
2024-01-23 19:56:09 +0800

    Add sharder to indexer

--- projects/liars_poker_bot/crates/card_platypus/src/database/indexer.rs
+++ projects/liars_poker_bot/crates/card_platypus/src/database/indexer.rs
@@ -2,11 +2,15 @@ use boomphf::Mphf;
 use games::{
     gamestates::{
         bluff::Bluff,
-        euchre::{ismorphic::normalize_euchre_istate, iterator::EuchreIsomorphicIStateIterator},
+        euchre::{
+            actions::EAction, ismorphic::normalize_euchre_istate,
+            iterator::EuchreIsomorphicIStateIterator,
+        },
         kuhn_poker::KuhnPoker,
     },
     istate::IStateKey,
     iterator::IStateIterator,
+    Action,
 };
 use itertools::Itertools;
 
@@ -16,19 +20,24 @@ const GAMMA: f64 = 1.7;
 
 pub struct Indexer {
     phf: Mphf<IStateKey>,
-    len: usize,
-    normalizer: fn(&IStateKey) -> IStateKey,
+    shard_len: usize,
+    num_shards: usize,
+    /// Returns the normalized istatekey and the associated shard
+    /// Shards can be used to keep similar istates near each other in the database
+    sharder: fn(&IStateKey) -> Option<(usize, IStateKey)>,
 }
 
 impl Indexer {
     /// May return None if the key isn't in the original function, but this isn't guaranteed
     pub fn index(&self, key: &IStateKey) -> Option<usize> {
-        let normed = (self.normalizer)(key);
-        self.phf.try_hash(&normed).map(|x| x as usize)
+        let (shard, normed) = (self.sharder)(key)?;
+        self.phf
+            .try_hash(&normed)
+            .map(|x| x as usize + (shard * self.shard_len))
     }
 
     pub fn len(&self) -> usize {
-        self.len
+        self.shard_len * self.num_shards
     }
 
     #[must_use]
@@ -45,12 +54,12 @@ impl Indexer {
         // find the nth item for the iterator -- a common call in later rounds of the phf.
         let istates = MMapVec::from_iter(istate_iter);
         let phf = Mphf::new(GAMMA, &istates);
-        let n = istates.len();
 
         Self {
             phf,
-            len: n,
-            normalizer: normalize_euchre_istate,
+            shard_len: istates.len(),
+            num_shards: 6, // one for each possible face up card
+            sharder: euchre_sharder,
         }
     }
 
@@ -60,8 +69,9 @@ impl Indexer {
         let phf = Mphf::new(GAMMA, &istates);
         Self {
             phf,
-            len: istates.len(),
-            normalizer: |x| *x,
+            shard_len: istates.len(),
+            num_shards: 1,
+            sharder: |x| Some((0, *x)),
         }
     }
 
@@ -71,8 +81,30 @@ impl Indexer {
         let phf = Mphf::new(GAMMA, &istates);
         Self {
             phf,
-            len: istates.len(),
-            normalizer: |x| *x,
+            shard_len: istates.len(),
+            num_shards: 1,
+            sharder: |x| Some((0, *x)),
         }
     }
+}
+
+fn euchre_sharder(istate: &IStateKey) -> Option<(usize, IStateKey)> {
+    let mut normed = normalize_euchre_istate(istate);
+    let face_up = *normed.get(5)?;
+    normed.swap(Action::from(EAction::NS), face_up); // swap to be an istate with ns as the face up card
+    normed.sort_range(0, 5.min(normed.len()));
+
+    let face_up = EAction::from(face_up);
+    use EAction::*;
+    let shard = match face_up {
+        NS => 0,
+        TS => 1,
+        JS => 2,
+        QS => 3,
+        KS => 4,
+        AS => 5,
+        _ => panic!("found non-spades face up card after normalization"),
+    };
+
+    Some((shard, normed))
 }
